f1a02aafcaf0677c141723a4eb8732310a65b836
Avetik <Kiteva4@gmail.com>
2020-06-06 21:18:31 +0300

    Изменения в части отображения элемента списка заявок (RequestRow)

--- Styles.js
+++ Styles.js
@@ -1,31 +1,77 @@
-import {StyleSheet} from 'react-native'
+import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-    main_screen_container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'skyblue'
-        
-    },
-    search_screen_container: {
-        flex: 1,
-        alignItems: "stretch",
-        justifyContent: 'center',
-        backgroundColor: 'skyblue'
-        
-    },
-    requestRow: {
-        flex: 1, 
-        alignItems:"center",
-        justifyContent: 'center',
-        backgroundColor: "white",
-        margin: 5,
-        flexDirection: 'column',
-        elevation: 2,
-        shadowColor: 'rgba(0,0,0, .4)',
-        shadowOffset: { height: 1, width: 1 },
-        shadowOpacity: 1,
-        shadowRadius: 1,
-    },
-})
+  main_screen_container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'skyblue',
+  },
+  search_screen_container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: 'skyblue',
+  },
+});
+
+export const stl_rqst_row = StyleSheet.create({
+  requestRowContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    margin: 5,
+    elevation: 2,
+    shadowColor: 'rgba(0,0,0, .4)',
+    shadowOffset: {height: 1, width: 1},
+    shadowOpacity: 1,
+    shadowRadius: 1,
+  },
+  requestRowLeftContent: {
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    margin: 2,
+  },
+  requestRowRightContent: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    margin: 2,
+  },
+  requestRowIDText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  requestRowAdressText: {
+    textAlign: 'left',
+    textAlignVertical: 'center',
+    fontWeight: 'bold',
+    color: '#2F80ED',
+  },
+  requestRowDateText: {
+    fontSize: 14,
+    color: 'grey',
+  },
+  requestRowCompanyText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'right',
+    textAlignVertical: 'center',
+    color: 'grey',
+  },
+  requestRowSpecText: {
+    color: 'grey',
+    textAlign: 'right',
+    textAlignVertical: 'center',
+    fontWeight: 'bold',
+  },
+  requestRowTypeText: {
+    fontSize: 14,
+    textAlign: 'right',
+    textAlignVertical: 'center',
+    fontWeight: 'bold',
+    color: '#56CCF2',
+  },
+});
--- src/Components/RequestRow.js
+++ src/Components/RequestRow.js
@@ -1,21 +1,29 @@
-import React from 'react'
-import { TouchableOpacity, Text } from 'react-native'
-import { styles } from '../../Styles'
-export default class RequestRow extends React.Component{
+import React from 'react';
+import {TouchableOpacity, Text, View} from 'react-native';
+import { stl_rqst_row} from '../../Styles';
 
-    render() {
-        return (
-            <TouchableOpacity
-                style={styles.requestRow}
-                onPress = { ()=> console.log('Row item pressed')}
-            >
-                <Text> id: {this.props.id           } </Text>
-                <Text> тип работ: {this.props.type  } </Text>
-                <Text> вид работ: {this.props.spec  } </Text>
-                <Text> адрес: {this.props.address   } </Text>
-                <Text> фирма: {this.props.client    } </Text>
-                <Text> дата: {this.props.date       } </Text> 
-            </TouchableOpacity>
-        );
-      }
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export default class RequestRow extends React.Component {
+  render() {
+    return (
+      <TouchableOpacity
+        style={stl_rqst_row.requestRowContainer}
+        onPress={() => console.log('Row item pressed')}>
+        <View style={stl_rqst_row.requestRowLeftContent}>
+          <Text style = {stl_rqst_row.requestRowIDText}> #{this.props.id} </Text>
+          <View style = {{ flexDirection: 'row', alignItems: 'center' }} >
+            <Icon name= "map-marker" size={18} color="#2F80ED" />
+            <Text numberOfLines={1} maxFontSizeMultiplier = {1.2} style = {stl_rqst_row.requestRowAdressText}> {this.props.address} </Text>
+          </View>
+          <Text style = {stl_rqst_row.requestRowDateText}> {this.props.date} </Text>
+        </View>
+        <View style={stl_rqst_row.requestRowRightContent}>
+          <Text style = {stl_rqst_row.requestRowCompanyText}> {this.props.client} </Text>
+          <Text adjustsFontSizeToFit numberOfLines={1} style = {stl_rqst_row.requestRowSpecText}> {this.props.spec} </Text>
+          <Text adjustsFontSizeToFit numberOfLines={1} style = {stl_rqst_row.requestRowTypeText}> {this.props.type} </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
 }
